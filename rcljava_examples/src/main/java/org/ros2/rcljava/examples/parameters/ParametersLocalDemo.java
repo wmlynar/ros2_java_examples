58464341d6a0308b7e433dad456f90e08132b3cd
Wojciech Mlynarczyk <wmlynarczyk@inovatica.com>
2019-02-16 13:30:48 +0100

    Added support for parameter callback

--- rcljava_examples/src/main/java/org/ros2/rcljava/examples/parameters/ParametersLocalDemo.java
+++ rcljava_examples/src/main/java/org/ros2/rcljava/examples/parameters/ParametersLocalDemo.java
@@ -1,6 +1,4 @@
-/* Copyright 2016 Esteve Fernandez <esteve@apache.org>
- * Copyright 2016-2017 Mickael Gaillard <mick.gaillard@gmail.com>
- *
+/* 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -28,8 +26,8 @@ import rcl_interfaces.msg.ListParametersResult;
 import rcl_interfaces.msg.ParameterDescriptor;
 import rcl_interfaces.msg.SetParametersResult;
 
-public class ParametersDemo {
-  private static final String NODE_NAME = ParametersDemo.class.getSimpleName().toLowerCase();
+public class ParametersLocalDemo {
+  private static final String NODE_NAME = ParametersLocalDemo.class.getSimpleName().toLowerCase();
 
   public static void main(String[] args) {
     // Initialize RCL
@@ -39,11 +37,9 @@ public class ParametersDemo {
     Node node = RCLJava.createNode(NODE_NAME, context);
 
     // set parameters
-    
     List<SetParametersResult> setParametersResults = node.setParameters(
         Arrays.<ParameterVariant>asList(new ParameterVariant("foo", 2L), new ParameterVariant("bar", "hello"),
             new ParameterVariant("baz", 1.45), new ParameterVariant("foobar", true)));
-
     for (SetParametersResult result : setParametersResults) {
       if (!result.getSuccessful()) {
         System.out.println("Failed to set parameter: " + result.getReason());
@@ -51,7 +47,6 @@ public class ParametersDemo {
     }
 
     // get parameters
-    
     List<ParameterVariant> parameters = node.getParameters(Arrays.asList("foo", "baz"));
 
     for (ParameterVariant parameter : parameters) {
@@ -60,7 +55,6 @@ public class ParametersDemo {
     }
 
     // get parameter types
-    
     List<ParameterType> parameterTypes = node.getParameterTypes(Arrays.asList("foo", "baz"));
 
     for (ParameterType type : parameterTypes) {
@@ -68,23 +62,18 @@ public class ParametersDemo {
     }
 
     // list parameters
-    
     ListParametersResult listParametersResult = node.listParameters(Arrays.asList("foo", "baz"), 10);
-
     for (String str : listParametersResult.getNames()) {
       System.out.println("Parameter name: " + str);
     }
-
     for (String str : listParametersResult.getPrefixes()) {
       System.out.println("Parameter prefix: " + str);
     }
 
     // describe parameters
-    
     List<ParameterDescriptor> parameterDescriptions = node.describeParameters(Arrays.asList("foo", "baz"));
-
     for (ParameterDescriptor description : parameterDescriptions) {
-      System.out.println("Parameter description, name: " + description.getName() + ", type: " + description.getType());
+      System.out.println("Parameter description, name: " + description.getName() + ", type: " + ParameterType.fromByte(description.getType()));
     }
 
     node.dispose();
---
+++ rcljava_examples/src/main/java/org/ros2/rcljava/examples/parameters/ParametersServerDemo.java
@@ -0,0 +1,84 @@
+/* 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.ros2.rcljava.examples.parameters;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import org.ros2.rcljava.RCLJava;
+import org.ros2.rcljava.node.Node;
+import org.ros2.rcljava.parameters.ParameterCallback;
+import org.ros2.rcljava.parameters.ParameterType;
+import org.ros2.rcljava.parameters.ParameterVariant;
+import org.ros2.rcljava.parameters.client.SyncParametersClientImpl;
+import org.ros2.rcljava.parameters.service.ParameterServiceImpl;
+
+import rcl_interfaces.msg.ListParametersResult;
+import rcl_interfaces.msg.ParameterDescriptor;
+import rcl_interfaces.msg.SetParametersResult;
+
+public class ParametersServerDemo {
+  private static final String NODE_NAME = ParametersServerDemo.class.getSimpleName().toLowerCase();
+
+  public static void main(String[] args)
+      throws NoSuchFieldException, IllegalAccessException, InterruptedException, ExecutionException {
+    // Initialize RCL
+    long context = RCLJava.rclJavaInit(args);
+
+    // Let's create a new Node
+    Node node = RCLJava.createNode(NODE_NAME, context);
+
+    ParameterServiceImpl parametersService = new ParameterServiceImpl(node);
+    SyncParametersClientImpl parametersClient = new SyncParametersClientImpl(node);
+
+    // add callback on parameter change
+    node.setParameterCallback(new ParameterCallback() {
+      @Override
+      public SetParametersResult onParamChange(List<ParameterVariant> parameters) {
+        for (ParameterVariant parameter : parameters) {
+          System.out.println("Callback: " + parameter.getName() + " " + parameter.getTypeName() + " "
+              + parameter.getValueAsString() + " " + parameter.getType().toString());
+        }
+        SetParametersResult result = new SetParametersResult();
+        result.setSuccessful(true);
+        return result;
+      }
+    });
+
+    // set parameters
+    List<SetParametersResult> setParametersResults = parametersClient.setParameters(
+        Arrays.<ParameterVariant>asList(new ParameterVariant("foo", 2L), new ParameterVariant("bar", "hello"),
+            new ParameterVariant("baz", 1.45), new ParameterVariant("foobar", true)));
+
+    for (SetParametersResult result : setParametersResults) {
+      if (!result.getSuccessful()) {
+        System.out.println("Failed to set parameter: " + result.getReason());
+      }
+    }
+
+    // get parameters
+    List<ParameterVariant> parameters = parametersClient.getParameters(Arrays.asList("foo", "baz"));
+    for (ParameterVariant parameter : parameters) {
+      System.out.println(parameter.getName() + " " + parameter.getTypeName() + " " + parameter.getValueAsString() + " "
+          + parameter.getType().toString());
+    }
+
+    RCLJava.spin(node);
+
+    node.dispose();
+    RCLJava.shutdown(context);
+  }
+}
